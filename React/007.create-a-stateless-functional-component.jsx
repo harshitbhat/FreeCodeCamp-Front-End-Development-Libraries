aeeb2201711b3d4dcf5529ca2895fa1cf3dddc3d
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-21 16:05:43 +0530

    Create a Stateless Functional Component

---
+++ React/007.create-a-stateless-functional-component.jsx
@@ -0,0 +1,14 @@
+const MyComponent = function () {
+  // Change code below this line
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+        laudantium dolorem id tenetur error eligendi perferendis architecto eos
+        hic. Repudiandae aliquid quod officiis, iusto reprehenderit quaerat
+        accusantium consequuntur magnam? In?
+      </p>
+    </div>
+  );
+  // Change code above this line
+};
