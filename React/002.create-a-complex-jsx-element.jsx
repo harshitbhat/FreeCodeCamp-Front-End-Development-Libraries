8e7c3a22089010f1ae9eceafb9dc9620fc0a9106
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-21 15:55:56 +0530

    Create a Complex JSX Element

---
+++ React/002.create-a-complex-jsx-element.jsx
@@ -0,0 +1,16 @@
+const JSX = (
+  <div>
+    <h1>Some Title</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+      laudantium dolorem id tenetur error eligendi perferendis architecto eos
+      hic. Repudiandae aliquid quod officiis, iusto reprehenderit quaerat
+      accusantium consequuntur magnam? In?
+    </p>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </div>
+);
