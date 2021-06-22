d3d6fb822374f1b7da51aad0864933983e7bb681
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-22 21:57:45 +0530

    Render Conditionally from Props

---
+++ React/042.render-conditionally-from-props.jsx
@@ -0,0 +1,39 @@
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { fiftyFifty } = this.props;
+    return <div>{fiftyFifty ? <h1>You Win!</h1> : <h1>You Lose!</h1>}</div>;
+  }
+}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState) => {
+      // Complete the return statement:
+      return {
+        counter: prevState.counter + 1,
+      };
+    });
+  }
+  render() {
+    const expression = Math.random() >= 0.5; // Change this line
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        {/* Change code below this line */}
+        <Results fiftyFifty={expression} />
+        {/* Change code above this line */}
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+  }
+}
